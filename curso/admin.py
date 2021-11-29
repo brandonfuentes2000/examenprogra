9a3df705036abd914a66f7753c81f8a16a238bc6
brandon <brandonalexis845@gmail.com>
2021-11-29 16:19:41 -0600

    avance

--- curso/admin.py
+++ curso/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from curso.models import Alumno, AlumnoAdmin, Curso, CursoAdmin
 # Register your models here.
+admin.site.register(Alumno, AlumnoAdmin)
+admin.site.register(Curso, CursoAdmin)
